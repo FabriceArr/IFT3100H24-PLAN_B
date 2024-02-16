fb0a0bf1aefb7c358ca65920eb6f80f3c47be5e9
FabriceArr <78975091+FabriceArr@users.noreply.github.com>
2024-02-16 14:35:45 -0500

    Implem du system que chaque object guarde leur modif despace global en memoire + test pour undo redo

--- src/Object.cpp
+++ src/Object.cpp
@@ -7,6 +7,9 @@ Object::Object(ofNode object, string name = "Objet")
 	translation_temp = { 0.f, 0.f, 0.f };
 	rotation_temp = { 0.f, 0.f, 0.f };
 	scale_temp = { 0.f, 0.f, 0.f };
+
+	changes_buffer = deque<ofMatrix4x4>(10);
+	current_change = 0;
 }
 
 const ofNode* Object::getObject() const
@@ -42,5 +45,55 @@ void Object::setName(string name)
 
 bool Object::operator==(const Object& a) {
 	return (*this == a) ?  true :  false;
+}
+
+void Object::addChange(ofMatrix4x4 mat)
+{
+	//current change isnt the newest, so clean the top until it meets the current change, discarting previous ones
+	if (current_change != changes_buffer.size()-1) {
+		for (size_t i = 0; i < (changes_buffer.size() - current_change); i++)
+		{
+			changes_buffer.pop_front();
+		}
+	}
+
+
+	if (changes_buffer.size() == changes_buffer.max_size()) {
+		changes_buffer.pop_back();
+	}
+
+	changes_buffer.push_front(mat);
+	current_change = changes_buffer.size()-1;
+}
+
+ofMatrix4x4 Object::getCurrentChangeM()
+{
+	return (changes_buffer.at(current_change));
+}
+
+bool Object::undoChange()
+{
+	//no more changes to undo
+	if (current_change == 0) {
+		return false;
+	}
+	else {
+		current_change--;
+		return true;
+	}
+	
+	
+}
+
+bool Object::recoverChange()
+{
+	//no more changes to recover
+	if (current_change == changes_buffer.size()-1) {
+		return false;
+	}
+	else {
+		current_change++;
+		return true;
+	}
 }
 
