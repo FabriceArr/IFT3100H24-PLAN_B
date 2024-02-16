79355db89f481eefb11ef72240a79de8f9f73b73
FabriceArr <78975091+FabriceArr@users.noreply.github.com>
2024-02-16 00:25:42 -0500

    Debut de click to select fonctionality

---
+++ src/ClickTexture.h
@@ -0,0 +1,42 @@
+//implémentation des concept vue ici https://ogldev.org/www/tutorial29/tutorial29.html
+
+#pragma once
+#include "ofMain.h"
+#include <GL/glew.h>
+#include <stdio.h>
+#include <math.h>
+#include <glm/glm.hpp>
+
+
+class ClickTexture
+{
+private:
+    GLuint m_WVPLocation;
+    GLuint selection_frame_buffer = 0;
+    GLuint clickable_pixel_texture = 0;
+    GLuint selection_depth_texture = 0;
+    ofShader click_shader;
+
+public:
+    ClickTexture();
+
+    ~ClickTexture();
+    
+
+
+    bool Init(unsigned int WindowWidth, unsigned int WindowHeight);
+
+    void EnableWriting();
+
+    void DisableWriting();
+
+    unsigned int ReadPixel(unsigned int x, unsigned int y);
+
+    void SetWVP(const ofMatrix4x4& WVP);
+    
+    
+
+
+    
+};
+
