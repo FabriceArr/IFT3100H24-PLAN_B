fd0dc08477cde8c215e309308dc7e9bb72bae273
FabriceArr <78975091+FabriceArr@users.noreply.github.com>
2024-03-05 16:46:38 -0500

    Clean up

--- src/Element2D.h
+++ src/Element2D.h
@@ -7,16 +7,11 @@ class Element2D : public Object
 {
 private:
 	ofImage image;
-	ofVbo object_buffer, limit_box;
 
 public:
 	Element2D(string primitivetype, string path);
-	Element2D(string name, ofMesh mesh);
 	~Element2D();
 
 	void draw(bool highlight, bool animated = false);
-
-	void primitivesLimitBox(bool type);
-	void customBox(ofMesh mesh);
 
 };
