cf17974e4316e56ca94e5e0b1bbb8dec12367238
Frédéric Poirier <uk_jobs@hotmail.com>
2024-03-06 21:14:10 -0500

    retiré les hotkeys 1-5 du titre de l'app

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,7 +3,7 @@
 
 void Application::setup()
 {
-	ofSetWindowTitle("Plan B viewer (m d f 1-5 a t e c o space)");
+	ofSetWindowTitle("Plan B viewer (m d f a t e c o space)");
 
 	interface.setup();
 	interface.getPositionSliderValues();
