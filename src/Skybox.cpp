75ceb959d9e8447ebc99360c71068fc5984061bc
FabriceArr <78975091+FabriceArr@users.noreply.github.com>
2024-04-20 14:37:59 -0400

    removed unnecessary code

--- src/Skybox.cpp
+++ src/Skybox.cpp
@@ -60,20 +60,11 @@ Skybox::Skybox()
 void Skybox::setup()
 {
 
-    skybox.setupShaderFromFile(GL_VERTEX_SHADER, "skybox/Shaders/skybox.vert");
-    skybox.setupShaderFromFile(GL_FRAGMENT_SHADER, "skybox/Shaders/skybox.frag");
+    skybox.setupShaderFromFile(GL_VERTEX_SHADER, "skybox/Shaders/skyboxy.vert");
+    skybox.setupShaderFromFile(GL_FRAGMENT_SHADER, "skybox/Shaders/skyboxy.frag");
     skybox.linkProgram();
 
   
-    // skybox VAO
-    
-    glGenVertexArrays(1, &skyboxVAO);
-    glGenBuffers(1, &skyboxVBO);
-    glBindVertexArray(skyboxVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     // setup cubemap
     loadCubemap("skybox/skybox.png");
@@ -84,12 +75,9 @@ void Skybox::draw() {
     glDepthFunc(GL_LEQUAL);
 
     skybox.begin();
-    skybox.setUniform1i("skybox", skyboxVAO);
-    glBindVertexArray(skyboxVAO);
     glActiveTexture(GL_TEXTURE0);
     cubemap.drawCubeMap();
 
-    glDrawArrays(GL_TRIANGLES, 0, 36);
 
     glDepthFunc(GL_LESS);
     skybox.end();
